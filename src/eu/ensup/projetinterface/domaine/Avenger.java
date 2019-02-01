e7fcd3e7d42f15e47e6d36eb6780968a00be49cd
David TOUCHARD <david.touchard@hotmail.fr>
2019-02-01 10:30:17 +0100

    Nouveau commit

--- src/eu/ensup/projetinterface/domaine/Avenger.java
+++ src/eu/ensup/projetinterface/domaine/Avenger.java
@@ -9,7 +9,7 @@ public class Avenger {
 	private String costume;
 	private int age;
 	private String pouvoir;
-	
+	//test pour git
 	public Avenger(String nom, String costume, int age, String pouvoir) {
 		super();
 		this.nom = nom;
